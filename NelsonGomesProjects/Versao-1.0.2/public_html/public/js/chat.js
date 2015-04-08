222fe2e0bd22097967b1884cd258301561665e29
Nelson Gomes <nelsonmpg@hotmail.com>
2015-04-08 23:29:20 +0100

    no message

---
+++ NelsonGomesProjects/Versao-1.0.2/public_html/public/js/chat.js
@@ -0,0 +1,153 @@
+$(document).ready(function () {
+    // cria a ligação com o servidor que disponibiliza o socket
+    var socket = io.connect(window.location.href);
+
+    // Atribui a largura da textarea
+    $("#msg").css({
+        width: $(window).width() * 0.98
+    });
+
+    // dados enviadas pelo socket para o servidor
+   
+    $('#msg').on('keydown', function (event) {
+        if (event.which === 8 || event.which === 46) {
+            socket.emit('msgappend', {
+                'data': event.which,
+                'pos': ($('#msg').prop("selectionStart"))
+            });
+        }
+    });
+
+    $('#msg').on('keypress', function (event) {
+        socket.emit('msgappend', {
+            'data': event.which,
+            'pos': ($('#msg').prop("selectionStart"))
+        });
+    });
+
+    // dados recebidos pelo socket para o browser
+    socket.on('msgappend', function (data) {
+        if (data.data === 8 /* backspace*/ || data.data === 46 /* delete */) {
+            var posactual = $('#msg').prop("selectionStart");
+            var str = $("#msg").val();
+            var str1 = "";
+            if (data.data === 8) {
+                str1 = str.slice(0, data.pos - 1) + str.slice(data.pos);
+            } else if (data.data === 46) {
+                str1 = str.slice(0, data.pos) + str.slice(data.pos + 1);
+            }
+            $('#msg').val(str1);
+            if (posactual < data.pos) {
+                $('#msg').selectRange(posactual); // set cursor position
+            } else {
+                $('#msg').selectRange(posactual - 1);
+            }
+        } else {
+            appendAtCaret($('#msg'), data.pos, String.fromCharCode(data.data));
+        }
+    });
+
+});
+
+$.fn.selectRange = function (start, end) {
+    if (!end)
+        end = start;
+    return this.each(function () {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+};
+//$('#elem').selectRange(3, 5); // select a range of text
+//$('#elem').selectRange(3); // set cursor position
+
+function appendAtCaret($target, caret, $value) {
+    var value = $target.val();
+    if (caret != value.length) {
+        var startPos = $target.prop("selectionStart");
+        var scrollTop = $target.scrollTop;
+        $target.val(value.substring(0, caret) + $value + value.substring(caret, value.length));
+        $target.prop("selectionStart", startPos + $value.length);
+        $target.prop("selectionEnd", startPos + $value.length);
+        $target.scrollTop = scrollTop;
+    } else if (caret === 0) {
+        $target.val($value + value);
+    } else {
+        $target.val(value + $value);
+    }
+}
+
+//function Iniciar() {
+//    $('#msg').document.designMode = 'On';
+//}
+//
+//function recortar() {
+//    $('#msg').document.execCommand('cut', false, null);
+//}
+//
+//function copiar() {
+//    $('#msg').document.execCommand('copy', false, null);
+//}
+//
+//function colar() {
+//    $('#msg').document.execCommand('paste', false, null);
+//}
+//
+//function desfazer() {
+//    $('#msg').document.execCommand('undo', false, null);
+//}
+//
+//function refazer() {
+//    $('#msg').document.execCommand('redo', false, null);
+//}
+//
+//function negrito() {
+//    $('#msg').document.execCommand('bold', false, null);
+//}
+//
+//function italico() {
+//    $('#msg').document.execCommand('italic', false, null);
+//}
+//
+//function sublinhado() {
+//    $('#msg').document.execCommand('underline', false, null);
+//}
+//
+//function alinharEsquerda() {
+//    $('#msg').document.execCommand('justifyleft', false, null);
+//}
+//
+//function centralizado() {
+//    $('#msg').document.execCommand('justifycenter', false, null);
+//}
+//
+//function alinharDireita() {
+//    $('#msg').document.execCommand('justifyright', false, null);
+//}
+//
+//function numeracao() {
+//    $('#msg').document.execCommand('insertorderedlist', false, null);
+//}
+//
+//function marcadores() {
+//    $('#msg').document.execCommand('insertunorderedlist', false, null);
+//}
+//
+//function fonte(fonte) {
+//    if (fonte != '') {
+//        $('#msg').document.execCommand('fontname', false, fonte);
+//    }
+//}
+//
+//function tamanho(tamanho) {
+//    if (tamanho != '') {
+//        $('#msg').document.execCommand('fontsize', false, tamanho);
+//    }
+//}
