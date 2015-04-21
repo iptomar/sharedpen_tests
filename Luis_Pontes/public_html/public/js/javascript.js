808fab98fba2bb9b6bd56f6e38bac3405ac14a48
LuisPontes <luismarquespontes@gmail.com>
2015-04-21 16:40:44 +0100

    no message

--- Luis_Pontes/public_html/public/js/javascript.js
+++ Luis_Pontes/public_html/public/js/javascript.js
@@ -14,6 +14,24 @@ $.fn.getCursorPosition = function () {
     return pos;
 };
 
+$.fn.addNewText = function (val1, val2) {
+    this.append(
+            "<div class='p_msg_chat'>" +
+            "<p class = 'user_chat' > " +
+            val1 +
+            "</p> : <p class='msg_chat'>" +
+            val2 +
+            "</p></div>");
+};
+
+$.fn.addAllColors = function (lista) {
+    for (var i = 0, max = lista.length; i < max; i++) {
+        $(this).append('<option value="' +
+                lista[i][0] + '" style="background:' + lista[i][0] + '">' +
+                lista[i][1] + '</option>');
+    }
+};
+
 /**
  * coloca o curso na posicao especificada
  * @param {type} start Posicao inicial
@@ -39,6 +57,87 @@ $.fn.selectRange = function (start, end) {
     });
 };
 
+function ajustElements() {
+    $("#contentor").css({
+        height: $(window).height() * 0.90
+    });
+//    $(".txtTab").css({
+//        height: $("#contentor").height() * 0.895
+//    });
+}
+
+function actulizaTabs(tabsTxt, tabsID) {
+    var tamanho = tabsID.length;
+    for (i = 0; i < tamanho; i++) {
+        var idd = "#" + Addtab(tabsID);
+        $(idd).val(tabsTxt[i]);
+    }
+}
+function Addtab(tabsID) {
+
+    // Conta quantos <li>(separadores) hÃ¡ (menos 1 por causa do separador "+ PÃ¡g")
+    tabsID.length = ($('ul#tabs li').length) - 1;
+
+    // Adiciona um separador antes do Ãºltimo (linha <li></li> antes do last-child)
+    $('ul#tabs li:last-child').before(
+            '<li id="li' +
+            (tabsID.length + 1) +
+            '"><a href="#page' +
+            (tabsID.length + 1) +
+            '" role="tab" data-toggle="tab">Página ' +
+            (tabsID.length + 1) +
+            ' <button type="button" id=' +
+            (tabsID.length + 1) +
+            ' class="btn btn-warning btn-xs xtab"><span>x</span></button></a>');
+
+    // Adiciona a pÃ¡gina depois da Ãºltima pÃ¡gina (<div></div> markup after the last-child of the <div class="tab-content">)
+    $('div.tab-content').append(
+            '<div class="tab-pane fade" id="page' +
+            (tabsID.length + 1) +
+            '"><textarea class="txtTab form-control" id="msg' +
+            (tabsID.length + 1) +
+            '" rows=15></textarea></div>');
+
+    $("#msg" + (tabsID.length + 1)).css({
+        height: $("#contentor").height() * 0.82
+    });
+
+    return tabsID[tabsID.length] = "msg" + (tabsID.length + 1);
+}
+
+function removeTab(tabsID, liElem) { // FunÃ§Ã£o que remove separador com o numero de <li>
+    $('ul#tabs > li#li' + liElem).fadeOut(1000, function () {
+        $(this).remove(); // Apaga o <li></li>(separador) com um efeito fadeout
+    });
+    // TambÃ©m apaga o <div>(pÃ¡gina) correta dentro de <div class="tab-content">
+    $('div.tab-content div#page' + liElem).remove();
+          var i=1;
+
+        $('#tabs').children('li').each(function () {   
+
+           if($(this).attr('id') != "li-last" && $(this).attr('id') != $('ul#tabs > li#li' + liElem).attr('id') ){
+                $(this).attr('id', "li"+i);  
+                $(this).children('a').attr('href', "#page"+i); 
+
+                var button = $(this).children('a').children();           
+                $(this).children('a').text('Pagina '+i +" ").append(button);  
+                $(this).children('a').children('button').attr('id',i);
+                i++;
+            }
+            
+        });   
+        var i=0;
+         $('.tab-content').children('div').each(function () {
+
+             if( $(this).attr('id') !=  $('div.tab-content div#page' + liElem)){
+                $(this).attr('id',"page"+(i+1));
+                $(this).children('textarea').attr('id',"msg"+(i+1));
+                 i++;
+             }
+        }); 
+
+       delete tabsID[tabsID.indexOf("msg"+liElem)];
+}
 /**
  * Faz o calculo para de uma cor em RGB 
  * @param {type} hex
